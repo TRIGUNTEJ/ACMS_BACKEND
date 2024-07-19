14fc540fc6b6bb4ec24c4c3d089101d2e0b12b07
TRIGUNTEJ <triguncherry@gmail.com>
2024-07-20 00:16:12 +0530

    files updated

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://trigun-acms.netlify.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
 }));
